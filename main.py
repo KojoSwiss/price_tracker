1fd8d9634afa1952e66b58246aa927a88331d2fd
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-03-20 13:23:20 +0100

    SMTP added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import smtplib
+import os
+
+my_email = os.environ.get("EMAIL")
+password = os.environ.get("PASSWORD")
 
 URL = "https://www.amazon.de/-/en/Lamicall-Adjustable-Stand-Tablet-iPad/dp/B06XCD4PDF/ref=sr_1_3?crid=281ELNZCEGD30&dchild=1&keywords=ipad+halter+st%C3%A4nder&qid=1616236607&sprefix=ipad+holder%2Claunchpad%2C176&sr=8-3"
 
@@ -17,5 +22,26 @@ soup = BeautifulSoup(response.content, "lxml")
 price_text = soup.find(name='span', class_='a-size-medium a-color-price priceBlockBuyingPriceString')
 price = price_text.getText()
 price_split = float(price.split("€")[1])
-print(price_split)
+
+product_name = soup.find(name='span', id='productTitle', class_='a-size-large product-title-word-break').getText().strip()
+
+best_price = 15
+
+if price_split < best_price:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg=f"Subject:Time to Buy {product_name}\n\n This price low or equal to {price_split}")
+else:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg=f"Subject: Not Time to Buy {product_name}\n\n This price is {price_split}")
+
+
+
 
